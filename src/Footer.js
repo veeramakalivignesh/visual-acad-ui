dc11040ad2feff11530f3f366ec7053f50f798f4
Rohith Vutukuru <rohithvutukuru267@gmail.com>
2024-09-02 01:52:51 -0700

    footer changed

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
           <nav className="navbar-dark"
               style={{ background: "#1d1d1d", color:"lightgrey" }}>
               <div className='p-1 text-center' >
-                <span>&copy; {new Date().getFullYear()} Visual Acad AI &nbsp;&nbsp; mvignesh@iitdalumni.com &nbsp;&nbsp; +1-858-518-9851</span>
+                <span>&copy; {new Date().getFullYear()} Visual Acad AI &nbsp;&nbsp; mvignesh@iitdalumni.com &nbsp;&nbsp; rvutukuru@ucsd.edu</span>
               </div>
           </nav>
       </footer>
